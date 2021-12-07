1533fcbcbbb4c890116092253ef69c6e489128cd
Ivan <aagrach@gmail.om>
2021-12-07 20:26:42 +0300

    получилось

--- chair/orders/models.py
+++ chair/orders/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class SalesOrders(models.Model):
+class SalesOrder(models.Model):
     amount = models.IntegerField()
     description = models.CharField(max_length=255)
