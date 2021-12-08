bae3a89fe5c0fd523cf5f56b7a8cc4881d1f2216
Ivan <aagrach@gmail.om>
2021-12-08 11:19:01 +0300

    мы отрисовали страницу через жесткую отрисовку  теперь поробуем создать точку API,  и рисовать отдельно

--- chair/chair/settings.py
+++ chair/chair/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'orders',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'chair.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
